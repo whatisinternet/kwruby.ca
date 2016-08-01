4d04736b842698bd29529b0ad073273b131e9c2b
Whatisinternet <joshteeter@gmail.com>
2016-08-01 11:07:34 -0400

    Class cleanup and unification. Added documentation on how to access the API

--- lib/meetup_feed.rb
+++ lib/meetup_feed.rb
@@ -1,33 +1,35 @@
 # MeetupFeed
 # - Collects meetup info from meetup.com
+# - Invokes meetup v2 api
+# - See: https://github.com/cranieri/meetup_client for full list of methods
+# - v2 docs here: http://www.meetup.com/meetup_api/
 #
 # Author: Josh Teeter
 # Date: 2016-07-31
 #
 class MeetupFeed
-  def self.all_meetings
-    meetup_api = MeetupApi.new
-    events = meetup_api.events(
-      group_urlname: ENV['MEETUP_NAME'], status: 'upcoming', desc: 'desc'
-    )['results']
-    events << meetup_api.events(
-      group_urlname: ENV['MEETUP_NAME'], status: 'past', desc: 'desc'
-    )['results']
-    events.flatten!
-    events
-  end
+  attr_accessor :meetup_api
 
   def self.upcoming_meetings
-    meetup_api = MeetupApi.new
-    meetup_api.events(
+    meetup_api_method(
+      :events,
       group_urlname: ENV['MEETUP_NAME'], status: 'upcoming', desc: 'desc'
-    )['results']
+    )
   end
 
   def self.past_meetings
-    meetup_api = MeetupApi.new
-    meetup_api.events(
+    meetup_api_method(
+      :events,
       group_urlname: ENV['MEETUP_NAME'], status: 'past', desc: 'desc'
-    )['results']
+    )
+  end
+
+  def self.all_meetings
+    [upcoming_meetings, past_meetings].flatten!
+  end
+
+  def self.meetup_api_method(method, params)
+    @meetup_api ||= MeetupApi.new
+    @meetup_api.public_send(method.to_sym, params)['results']
   end
 end
